40f6775cbb57652cf0b39404a2c0c86524cdd790
harsh-05 <harshverma09022001@gmail.com>
2026-03-07 23:16:47 +0530

    fix: variable sizing plus card styling

--- src/ColumnCard.tsx
+++ src/ColumnCard.tsx
@@ -1,31 +1,35 @@
-import { act, useState } from "react";
-import { ThreeDotsHorizontal } from "./Icons";
+import { useState } from "react";
+import { AddIcon, ThreeDotsHorizontal } from "./Icons";
 import type { Column } from "./types";
 
-export function ColumnCard({col}: {col: Column}) {
-    return (
-      <div className="min-w-68 max-w-68 min-h-48 bg-sky-300 rounded-md p-2">
-        <div className="flex justify-between items-center mb-5">
-          <div className="text-md font-medium uppercase ">{col.name}</div>
-          <div className=" rounded-md hover:bg-black/20 p-1">
-            <ThreeDotsHorizontal></ThreeDotsHorizontal>
-          </div>
-            </div>
-            {/* Add Card Input Box... Like in trello */}
-            <div>
-
-            </div>
+export function ColumnCard({ col }: { col: Column }) {
+  return (
+    <div className="min-w-68 max-w-68  bg-sky-300 rounded-md p-2">
+      <div className="flex justify-between items-center mb-5">
+        <div className="text-md font-medium uppercase  pl-4">{col.name}</div>
+        <div className=" rounded-md hover:bg-black/20 p-1 inline-block">
+          <ThreeDotsHorizontal></ThreeDotsHorizontal>
+        </div>
       </div>
-    );
+      {/* Add Card Input Box... Like in trello */}
+      <AddTask></AddTask>
+    </div>
+  );
 }
 
-
 function AddTask() {
-    const [active, setActive] = useState(false);
+  const [active, setActive] = useState(false);
 
-    if (!active) {
-        return <div>
-
-        </div>
-    }
+  if (!active) {
+    return (
+      <button
+        onClick={() => {
+        //  setActive(true);
+        }}
+        className=" w-full min-h-8 pl-2  flex gap-2 items-center  rounded-md hover:bg-black/20 active:bg-black/40 shadow-md active:shadow-sm"
+      >
+        <AddIcon size="4"></AddIcon> Add Task
+      </button>
+    );
+  }
 }
--- src/Icons.tsx
+++ src/Icons.tsx
@@ -1,5 +1,5 @@
-export function AddIcon() {
-    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+export function AddIcon({ size }: {size?:string}) {
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-${size ? size : '6'}`}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </svg>
 
@@ -13,7 +13,7 @@ export function CrossIcon() {
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="size-4"
+        className="size-6"
       >
         <path
           strokeLinecap="round"
@@ -33,7 +33,7 @@ export function ThreeDotsHorizontal() {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="size-6"
+      className="size-5"
     >
       <path
         strokeLinecap="round"
