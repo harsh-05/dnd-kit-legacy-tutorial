481cdfee1ea26020f2472fc7c7a3e2544787f8f7
harsh-05 <harshverma09022001@gmail.com>
2026-03-23 22:22:33 +0530

    fix:fixed the dynamic problem

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -1,5 +1,7 @@
-export function AddIcon({ size }: {size?:string}) {
-    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-${size ? size : '6'}`}>
+
+
+export function AddIcon({ className }: { className?: string }) {
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </svg>
 
