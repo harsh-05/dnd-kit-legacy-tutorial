beb6eb5492f783aa27a2ce4f4b8479ee0c3f88b7
harsh-05 <harshverma09022001@gmail.com>
2026-03-27 21:48:07 +0530

    Feat:Added the Sortable Column with placeholder

--- src/ColumnCard.tsx
+++ src/ColumnCard.tsx
@@ -1,18 +1,47 @@
 import { useState } from "react";
 import { AddIcon, ThreeDotsHorizontal } from "./Icons";
 import type { Column } from "./types";
+import { useSortable } from "@dnd-kit/sortable";
+import {CSS} from "@dnd-kit/utilities"
 
 export function ColumnCard({ col }: { col: Column }) {
+
+      const { attributes, listeners, setNodeRef, transform, transition, isDragging } =
+        useSortable({
+          id: col.id, 
+          data: {
+            type: "column",
+            col
+          }
+        });
+  
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  
+
   return (
-    <div className="min-w-68 max-w-68  bg-sky-300 rounded-md p-2">
-      <div className="flex justify-between items-center mb-5">
-        <div className="text-md font-medium uppercase  pl-4">{col.name}</div>
-        <div className=" rounded-md hover:bg-black/20 p-1 inline-block">
-          <ThreeDotsHorizontal></ThreeDotsHorizontal>
+    <div
+      ref={setNodeRef}
+      style={style}
+      className={`${isDragging ? "bg-neutral-400" : "bg-sky-300"} min-w-68 max-w-68 rounded-md p-2`}
+    >
+      <div className={`${isDragging ? "opacity-0" : ""}`}>
+        <div
+          {...attributes}
+          {...listeners}
+          className="flex justify-between items-center mb-5"
+        >
+          <div className="text-md font-medium uppercase  pl-4">{col.name}</div>
+          <div className=" rounded-md hover:bg-black/20 p-1 inline-block">
+            <ThreeDotsHorizontal></ThreeDotsHorizontal>
+          </div>
         </div>
+        {/* Add Card Input Box... Like in trello */}
+        <AddTask></AddTask>
       </div>
-      {/* Add Card Input Box... Like in trello */}
-      <AddTask></AddTask>
     </div>
   );
 }
@@ -28,7 +57,7 @@ function AddTask() {
         }}
         className=" w-full min-h-8 pl-2  flex gap-2 items-center  rounded-md hover:bg-black/20 active:bg-black/40 shadow-md active:shadow-sm"
       >
-        <AddIcon size="4"></AddIcon> Add Task
+        <AddIcon className="size-4"></AddIcon> Add Task
       </button>
     );
   }
--- src/KanbanBoard.tsx
+++ src/KanbanBoard.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import type { Column } from "./types";
 import { AddColumn } from "./AddColumn";
 import { ColumnCard } from "./ColumnCard";
+import { DndContext, DragOverlay, type DragStartEvent } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
 
 export function KanbanBoard() {
-    const [column, setColumn] = useState<Column[]>([]);
+  const [column, setColumn] = useState<Column[]>([]);
+  const colId = useMemo(() => column.map((col) => col.id), [column]);
+
+  const [activeEle, setActiveEle] = useState<Column | null>(null);
 
     function generateColumn(name: string) {
         const id = crypto.randomUUID();
@@ -12,14 +17,32 @@ export function KanbanBoard() {
     }
 
     return (
-        <div className="flex items-start gap-2 ">
-            {column.map((col:Column) => {
-                return <ColumnCard col= {col}></ColumnCard>
+      <div className="flex items-start gap-2 ">
+        <DndContext onDragStart={handleDragStart}>
+          <SortableContext items={colId}>
+            {column.map((col: Column) => {
+              return <ColumnCard key={col.id} col={col}></ColumnCard>;
             })}
-            
-            <AddColumn generateColumn={generateColumn}>{ }</AddColumn>
+          </SortableContext>
+
+          <DragOverlay>
+            {activeEle && <ColumnCard col={activeEle}></ColumnCard>}
+          </DragOverlay>
+          
+        </DndContext>
+
+        <AddColumn generateColumn={generateColumn}>{}</AddColumn>
       </div>
-    );
+  );
+  
+
+  function handleDragStart(event: DragStartEvent) {
+    console.log(event);
+    if (event.active.data.current?.type === "column") {
+      setActiveEle(event.active.data.current?.col);
+      return;
+    }
+  }
 }
 
 
