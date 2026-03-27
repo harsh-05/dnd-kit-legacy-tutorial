b0a725292a48a182f77b6e72f5e85a2ce69226e8
harsh-05 <harshverma09022001@gmail.com>
2026-03-27 22:23:20 +0530

    fix:minor changes

--- src/Draggable.tsx
+++ src/Draggable.tsx
@@ -1,5 +1,10 @@
-export function Draggable() {
+
+import type React from "react";
+
+export function Draggable({ children} :{children: React.ReactNode}) {
+     
+
     return <div>
-        
+     
     </div>
 }
