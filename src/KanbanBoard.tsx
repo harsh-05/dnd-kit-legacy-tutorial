42ba596683325a0a3067192b19702ed484159720
harsh-05 <harshverma09022001@gmail.com>
2026-03-27 22:22:16 +0530

    Fix:added the sorting logic on the dragEnd

--- src/KanbanBoard.tsx
+++ src/KanbanBoard.tsx
@@ -2,8 +2,8 @@ import { useMemo, useState } from "react";
 import type { Column } from "./types";
 import { AddColumn } from "./AddColumn";
 import { ColumnCard } from "./ColumnCard";
-import { DndContext, DragOverlay, type DragStartEvent } from "@dnd-kit/core";
-import { SortableContext } from "@dnd-kit/sortable";
+import { DndContext, DragOverlay, type DragEndEvent, type DragStartEvent } from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
 
 export function KanbanBoard() {
   const [column, setColumn] = useState<Column[]>([]);
@@ -18,7 +18,7 @@ export function KanbanBoard() {
 
     return (
       <div className="flex items-start gap-2 ">
-        <DndContext onDragStart={handleDragStart}>
+        <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
           <SortableContext items={colId}>
             {column.map((col: Column) => {
               return <ColumnCard key={col.id} col={col}></ColumnCard>;
@@ -41,6 +41,18 @@ export function KanbanBoard() {
     if (event.active.data.current?.type === "column") {
       setActiveEle(event.active.data.current?.col);
       return;
+    }
+  }
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event; 
+    if(over)
+    if (active.id !== over.id) {
+      setColumn((cols) => {
+        const oldIndex = column.findIndex((object => object.id === active.id));
+        const newIndex = column.findIndex((object => object.id === over.id));
+        return arrayMove(column, oldIndex, newIndex);
+        })
     }
   }
 }
