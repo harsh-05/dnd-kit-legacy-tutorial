b9a54dc005109cbf0227680c15f33aca115e673e
harsh-05 <harshverma09022001@gmail.com>
2026-04-11 19:52:09 +0530

    fix: fixed the Column dragging issue

--- src/KanbanBoard.tsx
+++ src/KanbanBoard.tsx
@@ -5,11 +5,15 @@ import { ColumnCard, ColumnCardPreview } from "./ColumnCard";
 import {
   DndContext,
   DragOverlay,
+  rectIntersection,
+  type CollisionDetection,
   type DragEndEvent,
+  type DragOverEvent,
   type DragStartEvent,
 } from "@dnd-kit/core";
 import { SortableContext, arrayMove } from "@dnd-kit/sortable";
 import { createPortal } from "react-dom";
+import { TaskCardPreview } from "./TaskCard";
 
 export function KanbanBoard() {
   const [column, setColumn] = useState<Column[]>([]);
@@ -17,13 +21,18 @@ export function KanbanBoard() {
 
   const colId = useMemo(() => column.map((col) => col.id), [column]);
 
-  const [activeEle, setActiveEle] = useState<Column | null>(null);
+  type ActiveElement =
+    | { type: "column"; element: Column }
+    | { type: "task"; element: Task }
+    | null;
+
+  const [activeEle, setActiveEle] = useState<ActiveElement>(null);
 
   const [colTaskName, setColTaskName] = useState<Task | undefined>(undefined);
-  
+
   useEffect(() => {
     console.log(tasks);
-  }, [tasks])
+  }, [tasks]);
 
   function generateColumn(name: string) {
     const id = crypto.randomUUID();
@@ -31,16 +40,21 @@ export function KanbanBoard() {
   }
 
   function generateTask() {
-    if (colTaskName && colTaskName.taskName !== '') {
+    if (colTaskName && colTaskName.taskName !== "") {
       setTasks((prev) => [...prev, colTaskName]);
-      
     }
     setColTaskName(undefined);
   }
 
+
   return (
     <div className="flex items-start gap-2 h-full overflow-x-auto">
-      <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+      <DndContext
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+        onDragOver={handleDragOver}
+        collisionDetection={kanbanCollisionDetection}
+      >
         <SortableContext items={colId}>
           {column.map((col: Column) => {
             return (
@@ -52,7 +66,7 @@ export function KanbanBoard() {
                 }
                 setColTaskName={setColTaskName}
                 generateTask={generateTask}
-                tasks={tasks.filter((task)=>(task.colId === col.id))}
+                tasks={tasks.filter((task) => task.colId === col.id)}
               ></ColumnCard>
             );
           })}
@@ -60,14 +74,22 @@ export function KanbanBoard() {
 
         {createPortal(
           <DragOverlay>
-            {activeEle && (
+            {activeEle && activeEle.type === "column" && (
               <ColumnCardPreview
-                col={activeEle}
+                col={activeEle.element}
                 coltaskName={
-                  colTaskName?.colId === activeEle.id ? colTaskName : undefined
+                  colTaskName?.colId === activeEle.element.id
+                    ? colTaskName
+                    : undefined
                 }
-                tasks={tasks.filter((task)=>task.colId === activeEle.id)}
+                tasks={tasks.filter(
+                  (task) => task.colId === activeEle.element.id,
+                )}
               ></ColumnCardPreview>
+            )}
+
+            {activeEle && activeEle.type === "task" && (
+              <TaskCardPreview task={activeEle.element}></TaskCardPreview>
             )}
           </DragOverlay>,
           document.body,
@@ -80,20 +102,57 @@ export function KanbanBoard() {
           generateTask();
         }}
       ></AddColumn>
-
     </div>
   );
 
   function handleDragStart(event: DragStartEvent) {
     console.log(event);
     if (event.active.data.current?.type === "column") {
-      setActiveEle(event.active.data.current?.col);
+      setActiveEle({ type: "column", element: event.active.data.current?.col });
+      return;
+    }
+     if (event.active.data.current?.type === "task") {
+      setActiveEle({ type: "task", element: event.active.data.current?.task });
       return;
     }
   }
 
+  function handleDragOver(event: DragOverEvent) {
+    console.log(event.active.data.current?.type);
+    console.log(event.over?.data.current?.type);
+
+    // console.log(event);
+    // const { active, over } = event;
+    // if (over && active.data.current?.type === 'task') { 
+    //   const oldIndex = tasks.findIndex((obj) => obj.id === active.id);
+    //       const newIndex = tasks.findIndex((obj) => obj.id === over.id);
+    //   if (active.id !== over.id ) {
+    //     setTasks((tasks) => {
+    //       tasks[oldIndex].colId = tasks[newIndex].colId;
+    //       return arrayMove(tasks, oldIndex, newIndex)
+    //     })
+      
+    //   }
+    //   if (over.data.current?.type === 'column') { 
+    //     setTasks((tasks) => {
+             
+    //       return tasks.map((task, ind) => {
+    //         if (ind === oldIndex) {
+    //           return {...task, colId: over.id}
+    //         }
+    //         return task;
+    //        })
+    //     })
+    //   }
+
+    // }
+
+    
+  }
   function handleDragEnd(event: DragEndEvent) {
     const { active, over } = event;
+
+    console.log(over?.data.current?.type);
     if (over)
       if (active.id !== over.id) {
         setColumn((cols) => {
@@ -105,4 +164,29 @@ export function KanbanBoard() {
 
     setActiveEle(null);
   }
+
+
+  function kanbanCollisionDetection(args: any) {
+    const { active, droppableContainers } = args;
+
+    // If the item we are dragging is a COLUMN...
+    if (active.data.current?.type === "column") {
+      // Get the normal collisions
+      const collisions = rectIntersection(args);
+
+      // FILTER OUT any collision that is NOT a column
+      return collisions.filter((collision: any) => {
+        const container = droppableContainers.find(
+          (c: any) => c.id === collision.id,
+        );
+        return container?.data.current?.type === "column";
+      });
+    }
+
+    // If it's a task, just use the default behavior (allow hovering over tasks and columns)
+    return rectIntersection(args);
+  }
+
+
+  
 }
